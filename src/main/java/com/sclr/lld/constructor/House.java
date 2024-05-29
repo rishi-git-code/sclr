9c255e0b4d88cbb6db77de8087f46cbc7798d02b
lenovo <lenovo@LAPTOP-GRJF78B8>
2024-05-30 01:05:33 +0530

    copy constructer changes

--- src/main/java/com/sclr/lld/constructor/House.java
+++ src/main/java/com/sclr/lld/constructor/House.java
@@ -1,5 +1,7 @@
 package com.sclr.lld.constructor;
 
+import java.awt.DisplayMode;
+
 public class House {
 
 	int noOfRoom;
@@ -9,27 +11,41 @@ public class House {
 	String typeOfHouse;
 	int profit;
 
-	//default constructor
+	// default constructor
 	public House() {
 		System.out.println("You are inside default House");
 	}
 
 	public House(String name) {
-		//this.name = name;
-		System.out.println("You are inside " + this.name + " House: "+name);
+		// this.name = name;
+		System.out.println("You are inside " + this.name + " House: " + name);
 	}
 
-	//Parameterized constructor
+	// Parameterized constructor
 	public House(String name, int cp, int sp) {
 		System.out.println("You are inside " + name + " House");
 		this.profit = sp - cp;
 	}
 
-	//copy constructor
-	public House(House mohit) {
-		
+	// copy constructor
+	public House(House mohit, String name) {
+		this.name = name;
+		noOfRoom = mohit.noOfRoom;
+		cp = mohit.cp;
+		sp = mohit.sp;
+		typeOfHouse = mohit.typeOfHouse;
+		profit = mohit.profit;
+
 	}
 
-	
+	public void Display(House obj) {
+		System.out.println("name: " + obj.name);
+		System.out.println("cp: " + obj.cp);
+		System.out.println("sp: " + obj.sp);
+		obj.profit = obj.sp - obj.cp;
+		System.out.println("profit: " + obj.profit);
+		System.out.println("typeOfHouse: " + obj.typeOfHouse);
+		System.out.println("noOfRoom: " + obj.noOfRoom);
+	}
 
 }
--- src/main/java/com/sclr/lld/constructor/TestConstructor.java
+++ src/main/java/com/sclr/lld/constructor/TestConstructor.java
@@ -1,5 +1,7 @@
 package com.sclr.lld.constructor;
 
+import java.awt.DisplayMode;
+
 public class TestConstructor {
 
 	public static void main(String[] args) {
@@ -11,25 +13,20 @@ public class TestConstructor {
 		mohit.sp = 3500;
 		mohit.name = "Mohit";
 		
-		System.out.println("Name: "+mohit.name);
+		mohit.Display(mohit);
+		
 		House rishi = new House("rishi",3000,5500);
 		rishi.typeOfHouse = "5-BHK";
 		rishi.noOfRoom = 5;
 		rishi.cp = 3000;
 		rishi.sp = 5500;
-		System.out.println("She got profit of "+rishi.profit);
-		
-		System.out.println("Mohit:" +mohit.typeOfHouse);
-		
-		//copy
+//copy
 //		House rishi = mohit;
-//		
 //		rishi.name = "rishi";
-//		System.out.println("Mohit:"+mohit.name);
 		
-		House kshama = new House(mohit);
-		kshama.name = "rishi";
-		System.out.println("Mohit:"+mohit.name);
+		
+		House kshama = new House(mohit,"Kshama");// smae for other family members
+		kshama.Display(kshama);
 		
 	}
 
